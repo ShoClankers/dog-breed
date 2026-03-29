f3a4891866efe428261257c202372f842aa9465e
Thato <Nkadimeng.Thato@icloud.com>
2026-03-29 12:59:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+class Dog:
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+
+self.breed = breed
+
+def display_details(self):
+    print(f"Name: {self.name}")
+    print(f"Age: {self.age}")
+    print(f"Breed: {self.breed}")
+    print("_" * 20)
+
+dog1 = Dog("Buddy", 3, "Golden Retriever")
+dog2 = Dog("Max", 5, "German Shepherd")
+
+dog1.display_details()
+dog2.display_details()
